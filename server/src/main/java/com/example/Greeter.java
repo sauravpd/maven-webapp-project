d1bfe53a429497401fcb11db09ac01e0a2b5c92f
saurav <sauravpd58@gmail.com>
2018-09-01 12:18:37 +0530

    commit changes

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -5,16 +5,10 @@ package com.example;
  */
 public class Greeter {
 
-	/**
-	 * This is a constructor.
-	 */
-	public Greeter() {
+	/** This is a constructor.*/
+	public Greeter() {	}
 
-	}
-
-	/**
-	 * greet method
-	 */
+	/** greet method*/
 	public String greet(final String someone) {
 		return String.format("Hello, %s!", someone);
 	}
